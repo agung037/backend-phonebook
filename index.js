9b3243d8732fc5882bcb0a1c87f78418f391bf4e
agung037 <agungk878@gmail.com>
2022-03-14 13:24:38 +0700

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+/* eslint-disable linebreak-style */
 const express = require('express')
 const morgan = require('morgan')
-const res = require('express/lib/response')
 const cors = require('cors')
 require('dotenv').config()
 
@@ -13,23 +13,23 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 
 
 // query semua data person dari mongodb
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 
@@ -37,78 +37,78 @@ app.get('/api/persons', (request, response) => {
 // query specific data berdasarkan id
 app.get('/api/persons/:id', (request, response, next) => {
 
-    // temukan berdasarkan id
-    Person.findById(request.params.id).then(person => {
-        // jika ditemukan, berikan data
-        if(person){
-            response.json(person)
-        }else{
-            // jika tidak beriksan response error
-            response.status(404).end()
-        }
-    })
-    // kemudian jika ada response error 
+  // temukan berdasarkan id
+  Person.findById(request.params.id).then(person => {
+    // jika ditemukan, berikan data
+    if(person){
+      response.json(person)
+    }else{
+      // jika tidak beriksan response error
+      response.status(404).end()
+    }
+  })
+    // kemudian jika ada response error
     // akan ditangkap oleh express menggunakan error handler di bagian bawah
     .catch(error => next(error))
 
 })
 
 
-// dapatkan jumlah data dan tampilkan 
+// dapatkan jumlah data dan tampilkan
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        const count = persons.length
-        const date = new Date()
-        response.send(`phonebook has info for ${count} people <br> ${date}`)
-    })
+  Person.find({}).then(persons => {
+    const count = persons.length
+    const date = new Date()
+    response.send(`phonebook has info for ${count} people <br> ${date}`)
+  })
 })
 
 
 // menambahkan data ke mongodb
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    // memberitau jika body kosong
-    if(body.name === undefined){
-        return response.status(400).json({error : 'name missing'})
-    }
+  // memberitau jika body kosong
+  if(body.name === undefined){
+    return response.status(400).json({ error : 'name missing' })
+  }
 
-    // membentuk object Person 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  // membentuk object Person
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  // menyimpan ke mongodb dan menampilkan hasil penyimpanan
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-
-    // menyimpan ke mongodb dan menampilkan hasil penyimpanan
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 
 })
 
 // update data dari mongodb berdasarkan id
 app.put('/api/persons/:id', (request, response, next) => {
 
-    // secara default validasi di mongoose pada put request di nonaktifkan
-    // ini cara mengaktifkannya
-    const {name, number} = request.body
-    Person.findByIdAndUpdate(request.params.id, {name, number}, {new: true, runValidators:true, context: 'query'})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  // secara default validasi di mongoose pada put request di nonaktifkan
+  // ini cara mengaktifkannya
+  const { name, number } = request.body
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators:true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 // menghapus data dari mongodb
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
 })
 
@@ -116,17 +116,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 // membuat error handler
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if(error.name === 'CastError'){
-        return response.status(400).send({error: 'malfomated id'})
-    }else if(error.name === 'ValidationError'){
-        console.log("validasi error")
-        return response.status(400).json({error: error.message})
-    }
-    next(error)
+  console.error(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malfomated id' })
+  }else if(error.name === 'ValidationError'){
+    console.log('validasi error')
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`server run on port ${PORT}`))
--- models/person.js
+++ models/person.js
@@ -1,51 +1,51 @@
-const { del } = require('express/lib/application')
+/* eslint-disable linebreak-style */
 const mongoose = require('mongoose')
 
 // membentuk url, dan mengambilnya dari env variable
+// eslint-disable-next-line no-undef
 const url = process.env.MONGO_URI
-
 console.log(`trying to connect to ${url}`)
 
 // koneksi ke mongo db
 mongoose.connect(url)
-    .then(result => {
-        console.log("connected to MongoDB 🥭🥭!")
-    })
-    .catch(error => {
-        console.log("connection error :", error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB 🥭🥭!')
+  })
+  .catch(error => {
+    console.log('connection error :', error.message)
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s./0-9]*$/g.test(v),
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: (v) =>/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g.test(v),
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        minlength: [8, "number must be atleast 8 Character"],
-        maxlength: [20, "maximul length 20"],
-        required: [true, "phone number required"]
-    }
+    minlength: [8, 'number must be atleast 8 Character'],
+    maxlength: [20, 'maximul length 20'],
+    required: [true, 'phone number required']
+  }
 })
 
 // mengubah hasil query dari mongodb
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        // mengubah key _id menjadi id
-        returnedObject.id = returnedObject._id.toString()
+  transform: (document, returnedObject) => {
+    // mengubah key _id menjadi id
+    returnedObject.id = returnedObject._id.toString()
 
-        // menghapus key _id
-        delete returnedObject._id
+    // menghapus key _id
+    delete returnedObject._id
 
-        // menghapus key __v
-        delete returnedObject.__v
-    }
+    // menghapus key __v
+    delete returnedObject.__v
+  }
 })
 
 
